2794d2bff5c9193be1f9415892c99eff882d403e
stephenhky <stephenhky@yahoo.com.hk>
2017-04-19 16:55:42 -0400

    code initialization

---
+++ DataCleanup.R
@@ -0,0 +1,14 @@
+# loading libraries
+library(dplyr)
+library(textmineR)
+
+# What to do?
+# 1. removal of digit-only token
+
+# tokenization: stringi::stri_split_boundaries
+
+# Optional:
+# 1. Stemming
+# 2. Lemmatization
+
+vec<- c('R is useful.', 'totally')
